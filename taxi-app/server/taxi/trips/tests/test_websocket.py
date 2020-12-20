5ca74e00bb6cee5e8f42a7b1877899fea000d03b
Igor Znamensky <igor.znamensky@gmail.com>
2020-12-20 17:42:37 +0300

    Done: WebSockets - Part Two, Part 1, Chapter 7, Joining the Driver Pool Next: WebSockets - Part Two, Part 1, Chapter 7, Requesting a Trip

--- taxi-app/server/taxi/trips/tests/test_websocket.py
+++ taxi-app/server/taxi/trips/tests/test_websocket.py
@@ -7,15 +7,25 @@ from channels.layers import get_channel_layer
 from django.contrib.auth import get_user_model
 from channels.db import database_sync_to_async
 from rest_framework_simplejwt.tokens import AccessToken
+from django.contrib.auth.models import Group
 
 
 @database_sync_to_async
-def create_user(username, password):
+def create_user(username, password, group='rider'):
+	# Create user.
 	user = get_user_model().objects.create_user(
 		username=username,
 		password=password
 	)
+
+	# Create user group.
+	user_group, _ = Group.objects.get_or_create(name=group)
+	user.groups.add(user_group)
+	user.save()
+
+	# Create access token.
 	access = AccessToken.for_user(user)
+
 	return user, access
 
 
@@ -44,9 +54,13 @@ class TestWebSocket:
 
 	async def test_can_send_and_receive_messages(self, settings):
 		settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+		_, access = await create_user(
+			'test.user@example.com', 'pAssw0rd', 'driver'
+		)
+
 		communicator = WebsocketCommunicator(
 			application=application,
-			path='/taxi/'
+			path=f'/taxi/?token={access}'
 		)
 		connected, _ = await communicator.connect()
 		message = {
@@ -54,23 +68,6 @@ class TestWebSocket:
 			'data': 'This is a test message.',
 		}
 		await communicator.send_json_to(message)
-		response = await communicator.receive_json_from()
-		assert response == message
-		await communicator.disconnect()
-
-	async def test_can_send_and_receive_broadcast_messages(self, settings):
-		settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
-		communicator = WebsocketCommunicator(
-			application=application,
-			path='/taxi/'
-		)
-		connected, _ = await communicator.connect()
-		message = {
-			'type': 'echo.message',
-			'data': 'This is a test message.',
-		}
-		channel_layer = get_channel_layer()
-		await channel_layer.group_send('test', message=message)
 		response = await communicator.receive_json_from()
 		assert response == message
 		await communicator.disconnect()
@@ -83,4 +80,24 @@ class TestWebSocket:
 		)
 		connected, _ = await communicator.connect()
 		assert connected is False
+
+	async def test_join_driver_pool(self, settings):
+		settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+		_, access = await create_user(
+			'test.user@example.com', 'pAssw0rd', 'driver'
+		)
+		communicator = WebsocketCommunicator(
+			application=application,
+			path=f'/taxi/?token={access}'
+		)
+		connected, _ = await communicator.connect()
+		message = {
+			'type': 'echo.message',
+			'data': 'This is a test message.',
+		}
+		channel_layer = get_channel_layer()
+		await channel_layer.group_send('drivers', message=message)
+		response = await communicator.receive_json_from()
+		assert response == message
+		await communicator.disconnect()
 
